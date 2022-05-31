d17c3e060a2d88ea165ff8c1d0a57b89bed14a37
markfialko <markfialkofialko@gmail.com>
2022-06-01 00:19:33 +0500

    (scripts): added constant html elements

---
+++ src/blocks/elements/elements.js
@@ -0,0 +1,10 @@
+const headerContainer = document.querySelector(".header__container");
+const menuContainer = document.querySelector(".menu__container");
+const menuList = document.querySelector(".menu__list");
+const headerButton = document.querySelector(".header__button");
+const header = document.querySelector("header");
+const menuBody = document.querySelector(".menu__body");
+const menuButton = document.querySelector(".menu__icon");
+const width = window.screen.width;
+
+export {headerContainer,menuContainer,menuList,headerButton,header,menuBody,menuButton,width};
